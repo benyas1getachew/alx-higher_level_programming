6e800907ef02c438563d9ab59e14d450d1f7e57d
benyas1getachew <99409114+benyas1getachew@users.noreply.github.com>
2024-03-05 15:42:18 +0530

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,8 @@
 
 
 if __name__ == '__main__':
-    import urllib.requestrequest = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    import urllib.request
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as res:
         content = res.read()
         print("Body response:")
